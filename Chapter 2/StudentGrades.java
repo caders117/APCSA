7e625186dc49266c46b10e671d6a8f606abe4867
caders117 <31219010+caders117@users.noreply.github.com>
2017-08-21 10:58:07 -0700

    Create StudentGrades.java

---
+++ Chapter 2/StudentGrades.java
@@ -0,0 +1,19 @@
+public class StudentGrades {
+	
+	  public static void main(String[] args) {
+		
+		    int lab, bonus;
+		    System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		    System.out.println("==          Student Points          ==");
+		    System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+		    System.out.println("\nName\t\tLab\tBonus\tTotal");
+		    System.out.println("----\t\t---\t-----\t-----");
+		    System.out.println("Alfred\t\t" + (lab = 77) + "\t" + (bonus = 5) + "\t" + (lab + bonus));
+		    System.out.println("Burkhart\t" + (lab = 66) + "\t" + (bonus = 7) + "\t" + (lab + bonus));
+		    System.out.println("Pepjin\t\t" + (lab = 51) + "\t" + (bonus = 11) + "\t" + (lab + bonus));
+		    System.out.println("Riza\t\t" + (lab = 81) + "\t" + (bonus = 2) + "\t" + (lab + bonus));
+		    System.out.println("Kwabena\t\t" + (lab = 98) + "\t" + (bonus = 3) + "\t" + (lab + bonus));
+		
+	  }
+}
+
