f1cb0c0e3b73bd9d6dfc042102fb6e242a1ef7f2
caders117 <31219010+caders117@users.noreply.github.com>
2017-08-29 12:25:32 -0700

    Update CircleTest.java

--- Chapter 2/CircleTest.java
+++ Chapter 2/CircleTest.java
@@ -1,22 +1,49 @@
 import java.util.Scanner;
-
+/***************************************************************
+ * This is the driver program to test our Circle class
+ * Create 3 different circle objects with the radius supplied 
+ * by the user.
+ * @author Cade
+ * @versi0n August 29, 2017
+****************************************************************/
 public class CircleTest
 {
+    
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the radius: ");
+        
+        /*
+         * Prompt user for radius and create Circle 
+         * object.  Print area and circumference.
+         */
+        System.out.print("\fEnter the radius: ");
         double r = scan.nextDouble();
         Circle circle1 = new Circle(r);
         double area = circle1.getArea();
-        System.out.println("circle1 // radius = " + r + " area = " + area);
+        System.out.println("circle1 // radius = " + r + " area = " + area + " sq. units");
         double circumference = circle1.getCircumference();
-        System.out.println("circumference = " + circumference);
+        System.out.println("circumference = " + circumference + " units\n");
     
-        System.out.println("Enter another radius: ");
+        /*
+         * Prompt user for radius and create Circle 
+         * object.  Print area and circumference.
+         */
+        System.out.print("Enter another radius: ");
         r = scan.nextDouble();
         Circle circle2 = new Circle(r);
-        System.out.println("circle2 // area = " + circle2.getArea() + " circumference = " + circle2.getCircumference());
+        System.out.println("circle2 // area = " + circle2.getArea() + " sq. units\ncircumference = " 
+                            + circle2.getCircumference() + " units\n");
+        
+        /*
+         * Prompt user for radius and create Circle 
+         * object.  Print area and circumference.
+         */
+        System.out.print("Enter another radius: ");
+        r = scan.nextDouble();
+        Circle circle3 = new Circle(r);
+        System.out.println("circle3 // area = " + circle3.getArea() + " sq. units\ncircumference = " 
+                            + circle3.getCircumference() + " units\n");
     }
 }
 
