bc4943134ff13e41a2b8aec0e8517ebedf3c8685
caders117 <31219010+caders117@users.noreply.github.com>
2017-09-14 18:10:10 -0700

    Create DivideTwoGui.java

---
+++ Chapter 3/DivideTwoGui.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class DivideTwoGui
+{
+    public static void main(String[] args)
+    {
+      String input;
+      JOptionPane.showMessageDialog(null, "This program divides two numbers.");
+      input = JOptionPane.showInputDialog("Enter the numerator:");
+      int numerator = Integer.parseInt(input);
+      input = JOptionPane.showInputDialog("Enter the denominator:");
+      int denominator = Integer.parseInt(input);
+      if(denominator == 0)
+    	  JOptionPane.showMessageDialog(null, "Ummmm you can't divide by 0 bro");
+      else
+    	  JOptionPane.showMessageDialog(null, numerator + "/" + denominator + " = " +
+                                    (double) numerator/denominator);
+      System.exit(0);
+    }
+}
