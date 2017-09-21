37b356480eb088fcf98627c1ead78110b1ce56df
caders117 <31219010+caders117@users.noreply.github.com>
2017-09-21 12:18:57 -0700

    Update Factorial.java

--- Chapter 3/Factorial.java
+++ Chapter 3/Factorial.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
+/**
+ * This program prints the factorial of 
+ * a positive integer entered by the user.
+ * 
+ * @author Cade
+ * @version 9/21/2017
+ */
 
 public class Factorial
 {
     public static void main(String[] args)
     {
-      Scanner scan = new Scanner(System.in);
-      int num;
-      int total = 0;
-      
-      System.out.print("Enter a positive integer: ");
-      num = scan.nextInt();
-      
-      while(num <= 0){
-      System.out.print("Enter a POSITIVE integer: ");
-      num = scan.nextInt();
-      }
-      
-      if(num != 0){
-        total = 1;
-        
-        for(int i = 1; i <= num; i++)
-          total *= i;
-      }
-      
-      System.out.println(total);
+        Scanner scan = new Scanner(System.in);
+        int num;
+        int total = 0;
+
+        System.out.print("Enter a positive integer: ");
+        num = scan.nextInt();
+
+        while(num <= 0){
+            System.out.print("Enter a POSITIVE integer: ");
+            num = scan.nextInt();
+        }
+
+        if(num != 0){
+            total = 1;
+
+            for(int i = 1; i <= num; i++)
+                total *= i;
+        }
+
+        System.out.println(total);
     }
 }
