8c5084822a87bca77c6d3d944ecdd1f6d3de1e1f
caders117 <31219010+caders117@users.noreply.github.com>
2017-09-28 11:58:52 -0700

    Create CoinTest.java

---
+++ Chapter 4/CoinTest.java
@@ -0,0 +1,39 @@
+/********************************************************************
+ * This program simulates flipping a coin 100 times and records
+ * the longest run of landing on "Heads" in a row.
+ * 
+ * @author Cade
+ * @version 9/28/2017
+ *
+// ********************************************************************/
+
+public class CoinTest
+{
+    public static void main (String[] args)
+    {
+        final int FLIPS = 100; // number of coin flips
+
+        int currentRun = 0; // length of the current run of HEADS
+        int maxRun = 0;     // length of the maximum run so far
+
+        // Create a     coin object
+        Coin coin = new Coin();
+        // Flip the coin FLIPS times
+        for (int i = 0; i < FLIPS; i++)
+        {
+            // Flip the coin & print the result
+            coin.flip();
+            System.out.println(coin.toString());
+            // Update the run information
+            if(coin.toString().equals("Heads")) {
+                currentRun++;
+                if(currentRun > maxRun)
+                    maxRun++;
+            } else
+                currentRun = 0;
+        }
+
+        // Print the results
+        System.out.println("The maximum run of HEADS was " + maxRun);
+    }
+}
