b8b66dce6c5950d1851659e5b2524b4914ede3d3
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-03 11:59:33 -0700

    Update Coin.java

--- Chapter 4/Coin.java
+++ Chapter 4/Coin.java
@@ -1,54 +1,59 @@
 /*******************************************************************
  *  This class represents a coin that can be flipped.
  *  
- *  @author Lewis and Loftus
- *  @version 9/28/2017
+ *  @author Cade
+ *  @version 10/3/2017
  *
 // *******************************************************************/
 
 public class Coin
 {
-    public final int HEADS = 0;
-    public final int TAILS = 1;
+    // instance variables, each coin object has its own copy.
+    public static final int HEADS = 0;
+    public static final int TAILS = 1;
 
     private int face;
 
-    // ---------------------------------------------
-    //   Sets up the coin by flipping it initially.
-    // ---------------------------------------------
-    public Coin ()
+    /**
+     *   Constructor method.  This is invoked whin new Coin() is
+     *   executed.  Only executed one time.
+     *   Note:
+     *      1) name is exactly the same as the class name 'Coin'
+     *      2) does not have  a return 'type', not even 'void'
+    */
+    public Coin () //header (does not have a return type)
     {
-        flip();
+        flip(); // flow of control goes to flip() method
     }
 
-    // -----------------------------------------------
-    //   Flips the coin by randomly choosing a face.
-    // -----------------------------------------------
-    public void flip()
+    /**
+     * Method to simulate coin flipping.  Generates a 'heads' 
+     * or tails.
+     */
+    public void flip() //header, note, NO ; at end of header
     {
         face = (int) (Math.random() * 2);
     }
 
-    // ---------------------------------------------------------
-    //   Returns true if the current face of the coin is heads.
-    // ---------------------------------------------------------
+    /**
+     * Method to check if the coin is facing heads up.
+     * @return true if 'heads' false if 'tails'
+     */
     public boolean isHeads()
     {
         return (face == HEADS);
     }
 
-    // ----------------------------------------------------
-    //   Returns the current face of the coin as a string.
-    // ----------------------------------------------------
+    /**
+     * Returns a string representation of a Coin object.
+     * @return <font face="courier">heads</font> or <font face="courier">tails</font>
+     */
     public String toString()
     {
-        String faceName;
-
-        if (face == HEADS)
-            faceName = "Heads";
+        if (isHeads())
+            return "Heads";
         else
-            faceName = "Tails";
-
-        return faceName;
+            return "Tails";
+            
     }
 }
