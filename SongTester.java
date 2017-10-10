9ee7dd4516ae1d43a7a3b984cbd55b7558d4ad43
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-10 11:20:30 -0700

    Create SongTester.java

---
+++ SongTester.java
@@ -0,0 +1,15 @@
+
+public class Tester {
+	public static void main(String[] args){
+		Song s1 = new Song("How to play the flute",
+							"Macklemore",
+							209);
+		
+		Song s2 = new Song("Care too much",
+							"Witt Lowry",
+							196);
+		
+		System.out.println(s1 + "\n");
+		System.out.println(s2);
+	}
+}
