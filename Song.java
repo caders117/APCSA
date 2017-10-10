b02c6adf6aaf4052d0a0e04c987f03b4b1b38b4a
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-10 11:27:38 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -13,9 +13,9 @@ public class Song {
 	
 	/**
 	 * Constructor method that sets the song's data.
-	 * @param title
-	 * @param artist
-	 * @param length (Seconds)
+	 * @param t title of song as a String
+	 * @param a artist name as a String
+	 * @param l length (Seconds) as an int
 	 */
 	public Song(String t, String a, int l){
 		title = t;
@@ -24,8 +24,8 @@ public class Song {
 	}
 	
 	/**
-	 * 
-	 * @param Title
+	 * Sets the title of the song.
+	 * @param t title of the song as a String
 	 */
 	public void setTitle(String t){
 		title = t;
@@ -33,25 +33,42 @@ public class Song {
 	
 	/**
 	 * Returns a String representation of the song.
-	 * @return returns the title, artist, and length.
+	 * @return returns the title, artist, and length as 
+	 *         a nicely formatted String.
 	 */
 	public String toString(){
 		String song = title + " by " + artist + ", Length: " + getLength();
 		return song;
 	}
 	
+	/**
+	 * Sets the artist's name.
+	 * @param a name of artist as a String.
+	 */
 	public void setArtist(String a){
 		artist = a;
 	}
 	
+	/**
+	 * Method to get the artist's name.
+	 * @return name of artist as a String.
+	 */
 	public String getArtist(){
 		return artist;
 	}
 	
+	/**
+	 * Sets the length of the song.
+	 * @param l length of the song as a String.
+	 */
 	public void setLength(int l){
 		length = l;
 	}
 	
+	/**
+	 * Method to get the length of the song.
+	 * @return returns the length of the song formatted nicely.
+	 */
 	public String getLength(){
 		return length / 60 + ":" + (length % 60);
 	}
