5912de7366e658c9ceadb0530931beea3fcc3eb1
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-17 12:23:18 -0700

    Create FinchSecurity.java

---
+++ Chapter 4/FinchSecurity.java
@@ -0,0 +1,30 @@
+package Finch;
+
+import java.awt.Color;
+
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+public class FinchSecurity {
+	public static void main(String[] args){
+		Finch myFinch = new Finch();
+		System.out.println("connected");
+
+		while(true){
+			if(myFinch.isShaken()){
+				break;
+			} else if(myFinch.isObstacle()){
+				myFinch.setLED(255, 0, 0, 1000);
+				myFinch.setLED(0, 0, 0, 1000);
+			} else {
+				myFinch.setWheelVelocities(-100, 100);
+				myFinch.buzz(262, 1000);
+				myFinch.saySomething("AAAAAAAA");
+				myFinch.setLED(Color.GREEN, 1000);
+				myFinch.setLED(Color.BLUE, 1000);
+			}
+		}
+		
+		myFinch.quit();
+		System.exit(0);
+	}
+}
