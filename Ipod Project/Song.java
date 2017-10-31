924f97e28b2e8996de9cea92c4dc365f85eaa8f6
caders117 <31219010+caders117@users.noreply.github.com>
2017-10-31 12:21:08 -0700

    Create Song.java

---
+++ Ipod Project/Song.java
@@ -0,0 +1,97 @@
+import java.io.File;
+
+import javafx.scene.media.Media;
+
+/**
+ * This class represents a song with a title, artist,
+ * and length.  Included are getters and setters for 
+ * each of the variables.
+ * @author rich8125
+ * @version 10/10/2017
+ */
+public class Song {
+
+    private String title;
+    private String artist;
+    private int length;
+    private String fileName;
+
+    /**
+     * Constructor method that sets the song's data.
+     * @param t title of song as a String
+     * @param a artist name as a String
+     * @param l length (Seconds) as an int
+     */
+    public Song(String t, String a, int l,  String fn){
+        title = t;
+        artist = a;
+        length = l;
+        fileName = fn;
+    }
+
+    /**
+     * Sets the title of the song.
+     * @param t title of the song as a String
+     */
+    public void setTitle(String t){
+        title = t;
+    }
+
+    /**
+     * Returns a String representation of the song.
+     * @return returns the title, artist, and length as 
+     *         a nicely formatted String.
+     */
+    public String toString(){
+        String song = title + " by " + artist + ", Length: " + getLength();
+        return song;
+    }
+
+    /**
+     * Sets the artist's name.
+     * @param a name of artist as a String.
+     */
+    public void setArtist(String a){
+        artist = a;
+    }
+
+    /**
+     * Method to get the artist's name.
+     * @return name of artist as a String.
+     */
+    public String getArtist(){
+        return artist;
+    }
+
+    /**
+     * Sets the length of the song.
+     * @param l length of the song as a String.
+     */
+    public void setLength(int l){
+        length = l;
+    }
+
+    /**
+     * Method to get the length of the song.
+     * @return returns the length of the song formatted nicely.
+     */
+    public String getLength(){
+        return length / 60 + ":" + (length % 60);
+    }
+
+    /**
+     * Method to get the length of the song in seconds.
+     * @return returns the length of the song in seconds.
+     */
+    public int getSecs(){
+        return length;
+    }
+
+    public String getFileName(){
+        return fileName;
+    }
+
+    /*public void play(){
+        
+    }*/
+}
