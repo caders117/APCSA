88c5ae20ff8d9fa94655d7b55213ca809a624110
caders117 <31219010+caders117@users.noreply.github.com>
2017-11-02 12:23:38 -0700

    Update Ipod.java

--- Ipod Project/Ipod.java
+++ Ipod Project/Ipod.java
@@ -3,18 +3,20 @@ public class  IPod {
     private String name;
     private Song song;
     private Song[] songList = new Song[10];
+    private int place = 0;
     
     public IPod (String n) {
-        name = n;
+        name = n; 
     }
     
-    public void addSong(String t, String a, int s, String f, int place) {
+    public void addSong(String t, String a, int s, String f) {
         song = new Song(t, a, s, f);
         songList[place] = song;
+        place++;
     }
     
     public void playSong(int place){
-        Mp3Player mp3 = new Mp3Player("C:/Users/rich8125/Desktop/Ipod Project/Songs/" + songList[place].getFileName());
+        Mp3Player mp3 = new Mp3Player("C:/Users/rich8125/Downloads/" + songList[place-1].getFileName());
         mp3.play();
     }
     
@@ -27,7 +29,8 @@ public class  IPod {
         
         t = name + "'s iPod\n";
         for(int i = 0; i < songList.length; i++){
-            t += i + "    " + songList[i] + "\n";
+            if(songList[i] != null)
+                t += (i+1) + ".   " + songList[i] + "\n";
         }   
         return t;
     }
