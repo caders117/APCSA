fd56499f13a733ff4563d64ffd9bee898ff27382
caders117 <31219010+caders117@users.noreply.github.com>
2017-11-02 12:24:04 -0700

    Update Test.java

--- Ipod Project/Test.java
+++ Ipod Project/Test.java
@@ -6,15 +6,15 @@ public class Test {
         Scanner scan = new Scanner(System.in);
         IPod ipod = new IPod("Cade");
         
-        ipod.addSong("I want you back", "Jackson 5", 179, "I want you back.mp3", 0);
-        ipod.addSong("Bohemian Rhapsody", "Queen", 179, "Bohemian Rhapsody.mp3", 1);
-        ipod.addSong("You Should Be Dancing", "Bee Gees", 179, "You Should Be Dancing.mp3",  2);
-        ipod.addSong("Gucci Gang", "Lil Pump", 125, "Gucci Gang.mp3", 3);
+        ipod.addSong("I want you back", "Jackson 5", 179, "I want you back.mp3");
+        ipod.addSong("Bohemian Rhapsody", "Queen", 179, "Bohemian Rhapsody.mp3");
+        ipod.addSong("You Should Be Dancing", "Bee Gees", 179, "You Should Be Dancing.mp3");
+        ipod.addSong("Gucci Gang", "Lil Pump", 125, "Gucci Gang.mp3");
         
         System.out.println(ipod);
         System.out.print("Which song do you want to play? ");
         int song = scan.nextInt();
         ipod.playSong(song);
-        System.out.println("Playing " + ipod.getSong(song));
+        System.out.println("Playing " + ipod.getSong(song-1));
     }
 }
