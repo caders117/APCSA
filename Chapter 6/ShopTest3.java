de607b4732662b8400f1e9a07c9a2f8dd1bf8340
caders117 <31219010+caders117@users.noreply.github.com>
2017-11-09 12:10:02 -0800

    Create ShopTest3.java

---
+++ Chapter 6/ShopTest3.java
@@ -0,0 +1,55 @@
+package ch06;
+/**********************************************************************
+ * Represents a shopping cart as an array of items
+ * 
+ * @author Cade
+ * @version 11/9/17
+ **********************************************************************/
+import java.text.NumberFormat;
+public class ShoppingCart
+{
+    private int itemCount; // total number of items in the cart
+    private double totalPrice; // total price of items in the cart
+    private int capacity; // current cart capacity
+    private Item[] cart;
+    
+    /**
+     * Creates an empty shopping cart with a capacity of 5 items.
+     */
+    public ShoppingCart()
+    {
+        capacity = 5;
+        itemCount = 0;
+        totalPrice = 0.0;
+        cart = new Item[5];
+    }
+    
+    /**
+     * Adds an item to the shopping cart.
+     */
+    public void addToCart(String itemName, double price, int quantity)
+    {
+        cart[itemCount] = new Item(itemName, price, quantity);
+        totalPrice += price * quantity;
+        itemCount++;
+
+    }
+    
+    /**
+     * Returns the contents of the cart together with
+     * summary information.
+     * 
+     * @return a nicely formatted summary of the cart
+     */
+    public String toString()
+    {
+        NumberFormat fmt = NumberFormat.getCurrencyInstance();
+        String contents = "\nS h o p p i n g  C a r t\n";
+        contents += "\nItem\t\tUnit Price\tQuantity\tTotal\n";
+        for (int i = 0; i < itemCount; i++)
+            contents += cart[i].toString() + "\n";
+        contents += "\nPlease pay: " + fmt.format(totalPrice);
+        contents += "\n";
+        return contents;
+    }
+}
