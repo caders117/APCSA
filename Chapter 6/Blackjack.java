4d9e8a352ed9c1569df1fc910a2a183e469518b8
caders117 <31219010+caders117@users.noreply.github.com>
2017-11-16 12:07:26 -0800

    Update Blackjack.java

--- Chapter 6/Blackjack.java
+++ Chapter 6/Blackjack.java
@@ -12,22 +12,41 @@ public class Blackjack
     {
         DeckOfCards masterDeck = new DeckOfCards();
         Scanner scan = new Scanner(System.in);
+        int playerTotal = 0, compTotal = 0;
         String playerAction;
-        System.out.println("Would you like to play BlackJack?");
+        System.out.println("\fWould you like to play BlackJack?");
         masterDeck.shuffle(); // put Card objects in random order
         ArrayList<Card> playerDeck = new ArrayList<>();
         ArrayList<Card> compDeck = new ArrayList<>();
         for(int i = 0; i < 2; i++){
             playerDeck.add(masterDeck.dealCard());
+            System.out.println(masterDeck.dealCard().getValue());
+            System.out.println(playerTotal += playerDeck.get(playerDeck.size() - 1).getValue());
             compDeck.add(masterDeck.dealCard());
+            compTotal += compDeck.get(compDeck.size() - 1).getValue();
         }
         System.out.println("Your cards are: " + playerDeck.get(0) + " and " + playerDeck.get(1));
+        if(playerTotal == 21)
+            System.out.println("BlackJack!");
         System.out.print("(H)it or (S)tand: ");
         playerAction = scan.next();
-        if(playerAction.equalsIgnoreCase("H"))
-            playerDeck.add(masterDeck.dealCard());
-        else if(playerAction.equalsIgnoreCase("S"))
-            //do computer stuff       
+        while(playerAction.equalsIgnoreCase("H")){
+            if(playerAction.equalsIgnoreCase("H")){
+                playerDeck.add(masterDeck.dealCard());
+                playerTotal += playerDeck.get(playerDeck.size() - 1).getValue();
+                System.out.print("Your cards are: ");
+                for(int i = 0; i < playerDeck.size(); i++)
+                    System.out.print(playerDeck.get(i) + (i != playerDeck.size() ? ", " : "\n"));
+                System.out.println(playerTotal);
+                if(playerTotal > 21){
+                    System.out.println("Busted");
+                    break;
+                }
+            } 
+            System.out.print("(H)it or (S)tand: ");
+            playerAction = scan.next();
+        }
+        
     }
-}
 
+}
