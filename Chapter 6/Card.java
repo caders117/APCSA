31173bb41ab9caf5f65d9e3bc419b1c11eb0913d
caders117 <31219010+caders117@users.noreply.github.com>
2017-11-16 12:09:11 -0800

    Update Card.java

--- Chapter 6/Card.java
+++ Chapter 6/Card.java
@@ -8,14 +8,24 @@ public class Card
 {
     private String face;
     private String suit;
-    public Card (String cardFace, String cardSuit)
+    private int value;
+    public Card (String cardFace, String cardSuit, int cardVal)
     {
         face = cardFace;
         suit = cardSuit;
+        value = cardVal;
     }
 
     public String toString(){
 
         return face + " of " + suit;
+    }
+    
+    public String getFace(){
+        return face;
+    }
+    
+    public int getValue(){
+        return value;
     }
 }
