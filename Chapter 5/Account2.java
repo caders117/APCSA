6254b83e3e14d2bb9a3f6634917e1bfa35356e8f
caders117 <31219010+caders117@users.noreply.github.com>
2018-01-23 11:39:25 -0800

    Create Account2.java

---
+++ Chapter 5/Account2.java
@@ -0,0 +1,87 @@
+//*******************************************************
+//Account.java
+//
+//A bank account class with methods to deposit to, withdraw from,
+//change the name on, and get a String representation
+//of the account.
+//*******************************************************
+public class Account
+{
+    private double balance;
+    private String name;
+    private int acctNum;
+    private static int nextAcctNum = 1000;
+    
+    public static int getNextNum(){
+        return nextAcctNum;
+    }
+    
+    public Account(double initBal, String owner, int num) {
+        balance = initBal;
+        name = owner;
+        acctNum = num;
+    }
+    
+    public Account(double initBal, String owner)
+    {
+        balance = initBal;
+        name = owner;
+        acctNum = getNextNum();
+        nextAcctNum++;
+    }
+    
+    public Account(String owner)
+    {
+        balance = 0;
+        name = owner;
+        acctNum = getNextNum();
+        nextAcctNum++;
+    }
+    
+    public String toString(){
+        return name + "'s account number is " + acctNum + ".  Balance is $" + balance + ".";
+        
+    }
+    //----------------------------------------------
+    //Checks to see if balance is sufficient for withdrawal.
+    //If so, decrements balance by amount; if not, prints message.
+    //----------------------------------------------
+    public void withdraw(double amount)
+    {
+        if (balance >= amount){
+            balance -= amount;
+            System.out.println("Withdraw $" + amount + " from " + name + "'s account.  New balance is $" + balance + ".");
+        } else
+            System.out.println("Insufficient funds");
+    }
+    
+    public void withdraw(double amount, double fee)
+    {
+        if (balance >= amount + fee) {
+            balance -= amount;
+            System.out.println("Withdraw $" + amount + " plus $" + fee + " fee from " + name + "'s account.  New balance is $" + balance + ".");
+        } else
+            System.out.println("Insufficient funds");
+    }
+    //----------------------------------------------
+    //Adds deposit amount to balance.
+    //----------------------------------------------
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+    //----------------------------------------------
+    //Returns balance.
+    //----------------------------------------------
+    public double getBalance()
+    {
+        return balance;
+    }
+    //----------------------------------------------
+    //Returns account name.
+    //----------------------------------------------
+    public String getName()
+    {
+        return name;
+    }
+}
