014c2bf152fbf331a8838fed1ef1b70ad16a9e53
caders117 <31219010+caders117@users.noreply.github.com>
2018-01-23 11:39:01 -0800

    Create AccountTest2.java

---
+++ Chapter 5/AccountTest2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AccountTest2 {
+    public static void main(String[] args){
+        String name;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\fHow many accounts would you like to be created? ");
+        int numAccts = scan.nextInt();
+        Account[] accts = new Account[numAccts];
+        for(int i = 0; i < numAccts; i++) {
+            System.out.print("\n\nEnter the name of Account #" + (i + 1) + ": ");
+            name = scan.next();
+            accts[i] = new Account(100, name);
+            System.out.println(accts[i]);
+            System.out.println("Next ID number is: " + Account.getNextNum());
+        }
+        
+        System.out.println("\nThank you bye.");
+    }
+}
+            
