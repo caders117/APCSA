ec8458af01bd29d2694b959e81d8aa6803a8aeb4
caders117 <31219010+caders117@users.noreply.github.com>
2018-01-25 11:48:13 -0800

    Update TransferAccount.java

--- Chapter 5/TransferAccount.java
+++ Chapter 5/TransferAccount.java
@@ -12,27 +12,57 @@ public class Account
     private int acctNum;
     private static int nextAcctNum = 1000;
     
+    /**
+     * Transfers money from one account to another
+     *
+     * @param acct account to transfer money from
+     * @param acct2 account to transfer money to
+     * @param amount amount of money to be transferred
+     */
     public static void transfer(Account acct, Account acct2, double amount) {
         acct.transfer(acct2, amount);
     }
-        
     
+    /**
+     * Transfers a specified amount of money to another account
+     * 
+     * @param acct Account to transfer money to
+     * @param amount amount of money to transfer
+     */
     public void transfer(Account acct, double amount) {
         this.withdraw(amount);
         acct.deposit(amount);
         System.out.println("Transferred " + amount + " from " + name + "'s account to " + acct.getName() + "'s account."); 
     }
     
+    /**
+     * returns the next Account Number
+     * 
+     * @returns next account number
+     */
     public static int getNextNum(){
         return nextAcctNum;
     }
     
+    /**
+     * Constructor -- initializes balance, owner, and account number
+     * 
+     * @param initBal the initial balance
+     * @param owner the name of the owner
+     * @param number account number
+     */
     public Account(double initBal, String owner, int num) {
         balance = initBal;
         name = owner;
         acctNum = num;
     }
     
+    /**
+     * Constructor -- initializes balance and owner and a random account number
+     * 
+     * @param initBal the initial balance
+     * @param owner the name of the owner
+     */
     public Account(double initBal, String owner)
     {
         balance = initBal;
@@ -41,6 +71,11 @@ public class Account
         nextAcctNum++;
     }
     
+    /**
+     * Constructor -- initializes owner and balance of 0 and random account number
+     * 
+     * @param owner the name of the owner
+     */
     public Account(String owner)
     {
         balance = 0;
@@ -49,14 +84,19 @@ public class Account
         nextAcctNum++;
     }
     
+    /**
+     * toString() override
+     */
     public String toString(){
         return name + "'s account number is " + acctNum + ".  Balance is $" + balance + ".";
         
     }
-    //----------------------------------------------
-    //Checks to see if balance is sufficient for withdrawal.
-    //If so, decrements balance by amount; if not, prints message.
-    //----------------------------------------------
+    /**
+     * Checks to see if balance is sufficient for withdrawal.
+     * If so, decrements balance by amount; if not, prints message.
+     * 
+     * @param amount amount to withdraw
+     */
     public void withdraw(double amount)
     {
         if (balance >= amount){
@@ -66,6 +106,13 @@ public class Account
             System.out.println("Insufficient funds");
     }
     
+    /**
+     * Checks to see if balance is sufficient for withdrawal.
+     * If so, decrements balance by amount; if not, prints message.
+     * 
+     * @param amount amount to withdraw
+     * @param fee fee to charge in addition to withdrawal
+     */
     public void withdraw(double amount, double fee)
     {
         if (balance >= amount + fee) {
@@ -74,23 +121,31 @@ public class Account
         } else
             System.out.println("Insufficient funds");
     }
-    //----------------------------------------------
-    //Adds deposit amount to balance.
-    //----------------------------------------------
+    
+    /**
+     * Adds deposit amount to balance.
+     * 
+     * @param amount amount to deposit
+     */
     public void deposit(double amount)
     {
         balance += amount;
     }
-    //----------------------------------------------
-    //Returns balance.
-    //----------------------------------------------
+    /**
+     * Returns balance.
+     * 
+     * @return balance
+     */
     public double getBalance()
     {
         return balance;
     }
-    //----------------------------------------------
-    //Returns account name.
-    //----------------------------------------------
+    
+    /**
+     * Returns account name.
+     * 
+     * @return name of owner
+     */
     public String getName()
     {
         return name;
