a5f9472402873aed3f63c0cb941c81f8ad27b5c9
caders117 <31219010+caders117@users.noreply.github.com>
2018-01-29 10:44:12 -0800

    Update Task.java

--- Chapter 5/Task.java
+++ Chapter 5/Task.java
@@ -1,27 +1,64 @@
+/**
+ * Task class represents a task that has a priority
+ * and can be compared to other tasks.
+ * 
+ * @author Cade
+ * @version 1/29/2018
+ */
 public class Task implements Priority, Comparable {
     int priority;
     String name;
     
+    /**
+     * Constructor that sets the name of the task
+     * 
+     * @param n name of task
+     */
     public Task(String n) {
         this.name = n;
     }
     
+    /**
+     * Getter to get the name of the task
+     * 
+     * @return name of the priority
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Sets this task's priority
+     * 
+     * @param p priority to set this task to
+     */
     public void setPriority(int p) {
         priority = p;
     }
     
+    /**
+     * Getter for this task's priority
+     * 
+     * @return priority
+     */
     public int getPriority() {
         return priority;
     }
     
+    /**
+     * toString() override method
+     * 
+     * @return String representation of the task
+     */
     public String toString() {
-        return "\"name\" has priority of " + priority;
+        return "\"" + name + "\" has priority of " + priority;
     }
     
+    /**
+     * Compares this Task to another Task based on priority
+     * 
+     * @param Task to be compared
+     */
     public int compareTo(Object o) {
         Task t = ((Task)o);
         if(this.priority > t.getPriority())
