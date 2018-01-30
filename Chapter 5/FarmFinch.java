6f0a26cfb0b2af43dcc5d0b6b9d68aea84c9baa7
caders117 <31219010+caders117@users.noreply.github.com>
2018-01-30 12:07:15 -0800

    Update FarmFinch.java

--- Chapter 5/FarmFinch.java
+++ Chapter 5/FarmFinch.java
@@ -1,14 +1,40 @@
 import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+import java.util.Scanner;
 
+/**
+ * Makes a Finch imitate different animals at the farm.
+ * 
+ * @author Cade
+ * @version 1/30/2018
+ */
 public class FarmFinch {
 
     public static Finch myFinch = new Finch();
 
     public static void main(String[] args) {
+        System.out.println("\fConnected");
+        Scanner scan = new Scanner(System.in);
+        String ans = "";
+        while(!ans.equals("quit")) {
+            System.out.println("\nWhich animal would you like the Finch to mimic?");
+            System.out.println("Cow, Horse, Duck, Sheep");
+            System.out.println("Type \"quit\" to quit");
+            ans = scan.next();
+            if(!playAnimal(ans))
+                if(!ans.equals("quit"))
+                    System.out.println("Invalid entry.  Try again.");
+        }        
+        System.out.println("Thank you! Bye.");
+        
         myFinch.quit();
         System.exit(0);
     }
 
+    /**
+     * Plays whatever animal is passed.  Returns false if no valid animal has been passed.
+     * 
+     * @returns false if no valid animal was passed and true if a valid animal was passed.
+     */
     public static boolean playAnimal(String animal) {
         animal = animal.toLowerCase();
         if(animal.equals("cow")) {
@@ -23,28 +49,41 @@ public class FarmFinch {
         } else if(animal.equals("sheep")) {
             sheep();
             return true;
-            return false;
         }
+        return false;
+
     }
 
+    /**
+     * Method to imitate a cow.
+     */
     public static void cow() {
-        myFinch.say("Moo");
-        myFinch.setWheelVelocities(100, 100, 2000);
+        myFinch.playClip("C:/Users/rich8125/Downloads/cowmoo.wav");
+        myFinch.setWheelVelocities(100, 100, 3000);
     }
 
+    /**
+     * Method to imitate a horse.
+     */
     public static void horse() {
-        myFinch.say("Neigh");
-        myFinch.setWheelVelocities(50, 100, 2000);
+        myFinch.playClip("C:/Users/rich8125/Downloads/horseneigh.wav");
+        myFinch.setWheelVelocities(255, 255, 2000);
     }
 
+    /**
+     * Method to imitate a duck.
+     */
     public static void duck() {
-        myFinch.say("Quack");
-        myFinch.setWheelVelocities(100, 110, 1000);
-        myFinch.setWheelVelocities(110, 100, 1000);
+        myFinch.playClip("C:/Users/rich8125/Downloads/duckquack.wav");
+        myFinch.setWheelVelocities(100, 120, 1500);
+        myFinch.setWheelVelocities(120, 100, 1500);
     }
 
+    /**
+     * Method to imitate a sheep.
+     */
     public static void sheep() {
-        myFinch.say("Baa");
-        myFinch.setWheelVelocities();
+        myFinch.playClip("C:/Users/rich8125/Downloads/sheepbleat.wav");
+        myFinch.setWheelVelocities(0, 100, 3000);
     }
 }
