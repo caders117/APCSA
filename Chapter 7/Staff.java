f7b6d6ed12420194a19b5864a7efd1cea4e6081b
caders117 <31219010+caders117@users.noreply.github.com>
2018-02-26 10:58:57 -0800

    Update Staff.java

--- Chapter 7/Staff.java
+++ Chapter 7/Staff.java
@@ -11,7 +11,7 @@ public class Staff
     //-----------------------------------------------------------------
     public Staff ()
     {
-        staffList = new StaffMember[6];
+        staffList = new StaffMember[8];
         staffList[0] = new Executive ("Sam", "123 Main Line",
             "555-0469", "123-45-6789", 2423.07);
         staffList[1] = new Employee ("Carla", "456 Off Line",
@@ -24,8 +24,16 @@ public class Staff
             "555-8374");
         staffList[5] = new Volunteer ("Cliff", "321 Duds Lane",
             "555-7282");
+        staffList[6] = new Commission ("Dave", "983 Street Street",
+            "555-3784", "289-12-3892", 6.25, .2)
+        staffList[7] = new Commission ("Dave", "983 Street Street",
+            "555-3784", "289-12-3892", 9.75, .15)
         ((Executive)staffList[0]).awardBonus (500.00);
-        ((Hourly)staffList[3]).addHours (40);
+        ((Hourly)staffList[6]).addHours (40);
+        ((Commission)staffList[6]).addHours(35);
+        ((Commission)staffList[6]).addSales(400);
+        ((Commission)staffList[7]).addHours(40);
+        ((Commission)staffList[7]).addSales(950);
     }
     //-----------------------------------------------------------------
     // Pays all staff members.
