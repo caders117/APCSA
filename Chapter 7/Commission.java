ca7b458c9b736704183be3a3490964ba7b78da5b
caders117 <31219010+caders117@users.noreply.github.com>
2018-02-27 11:46:46 -0800

    Update Commission.java

--- Chapter 7/Commission.java
+++ Chapter 7/Commission.java
@@ -2,6 +2,11 @@ public class Commission extends Hourly {
     private double totalSales;
     private double commRate;
     
+    /**
+     * Constructor -- creates Hourly with a rate of commission
+     * 
+     * @param commRate Rate of commission
+     */
     public Commission (String name,
                         String address, 
                         String phone, 
@@ -12,11 +17,20 @@ public class Commission extends Hourly {
         this.commRate = commRate;
     }
     
+    /**
+     * Adds sales to this staffmember's total sales
+     * 
+     * @param tSales sales to be added
+     */
     public void addSales(double tSales) {
         totalSales += tSales;
     }
     
+    /**
+     * Pays this staffmember
+     */
     public double pay() {
+        totalSales = 0;
         return totalSales * commRate + super.pay();
     }
 }
