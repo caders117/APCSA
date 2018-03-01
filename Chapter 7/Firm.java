73e3dc6f08d232111f1095b05259d70abbe8f181
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-01 11:36:25 -0800

    Update Firm.java

--- Chapter 7/Firm.java
+++ Chapter 7/Firm.java
@@ -10,6 +10,7 @@ public class Firm
      */
     public static void main (String[] args)
     {
+        System.out.println("\f");
         Staff personnel = new Staff();
         personnel.payday();
     }
