92c4d60d9f10587801d3a43f839e36ffe79c574b
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-04 00:26:28 -0800

    Update Game.java

--- Text Based Game/Game.java
+++ Text Based Game/Game.java
@@ -15,35 +15,19 @@ public class Game {
 		BigBird b1 = new BigBird("Big Bird", "A big, yellow, dangerous bird", 3, 3);
 		things.add(b1);
 		//Elmo elmo = new Elmo("Elmo", "ELMO!!", )
-		updateBoard(game, things);
+		game.updateBoard(things);
 		System.out.println("Welcome to [Game name]!");
 		System.out.println("Your goal is to kill enough Big Birds so that you can fight Elmo.");
 		System.out.println("Type 'help' for a list of commands.");
 		System.out.println("Type '<command> help' for further details on how to use the command.");
 		System.out.println("Good luck!");
 		System.out.println(things.get(0));
-		/*for(int i = 0; i < things.size(); i++) {
-			int itemX = things.get(i).getX();
-			int itemY = things.get(i).getY();
-			NamedThing thing = things.get(i);
-			game.getBoard().get(itemY).get(itemX).add(thing);
-		}*/
 		System.out.println(Arrays.toString(things.toArray()));
 
 		System.out.println(Arrays.toString(game.getBoard().toArray()));
-		System.out.println(contentsOfPos(game, p.getX(), p.getY()));
+		game.printBoard();
+		System.out.println(game.contentsOfPos(game, p.getX(), p.getY()));
 		//String input = scan.nextLine();
-	}
-	
-	
-	public static String contentsOfPos(GameBoard board, int x, int y) {
-		String contentStr = "Contents of position (" + x + ", " + y + "):\n";
-		NamedThing contentObj;
-		for(int i = 0; i < board.getBoard().get(y).get(x).size(); i++) {
-			contentObj = board.getBoard().get(y).get(x).get(i);
-			contentStr += contentObj.nameAndDesc() + "\n";
-		}
-		return contentStr;
 	}
 	
 	public static String output(String inputStr) {
@@ -64,15 +48,5 @@ public class Game {
 		return help;
 	}
 	
-	public static void updateBoard(GameBoard board, ArrayList<NamedThing> things) {
-		for(int i = 0; i < board.getBoard().size(); i++)
-			for(int j = 0; j < board.getBoard().get(i).size(); j++)
-				board.getBoard().get(i).get(j).clear();
-		for(int i = 0; i < things.size(); i++) {
-			int itemX = things.get(i).getX();
-			int itemY = things.get(i).getY();
-			NamedThing thing = things.get(i);
-			board.getBoard().get(itemY).get(itemX).add(thing);
-		}
-	}
+	
 }
