89c4d98d46f82aa89b8cf344a1007f257f0dd9d7
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-04 16:20:59 -0800

    Update Elmo.java

--- Text Based Game/Elmo.java
+++ Text Based Game/Elmo.java
@@ -1,8 +1,64 @@
 
+import java.util.*;
 public class Elmo extends Entity{
 
+	Scanner scan = new Scanner(System.in);
 	public Elmo(String name, String desc, int xPos, int yPos) {
 		super(name, desc, xPos, yPos);
 	}
-	
+
+	public void trivia() {
+		boolean b = true;
+		while(b= true) {
+			System.out.println("When was the first epidsode of Sesame Street aired?" +
+					"\nA.1969" + "\nB.1970" + "\nC.1968" + "\nD.2014");
+			String answer = scan.nextLine();
+			if (answer.equalsIgnoreCase("A")) {
+				correct();
+			}
+			else {
+				wrong();
+				break;
+			}
+			System.out.println("What state is Sesame Street filmed in?" +
+					"\nA.New York" + "\nB.California" + "\nC.Florida" + "\nD.Sesame Street is a myth");
+			answer = scan.nextLine();
+			if (answer.equalsIgnoreCase("A")) {
+				correct();
+			}
+			else {
+				wrong();
+				break;
+			}
+			System.out.println("When was Jerome the Walrus introduced into the show?" +
+					"\nA.1969" + "\nB.1982" + "\nC.2017" + "\nD.Jerome is not a character");
+			answer = scan.nextLine();
+			if (answer.equalsIgnoreCase("D")) {
+				correct();
+			}
+			else {
+				wrong();
+				break;
+			}
+			System.out.println("How tall is Big Bird?" +
+					"\nA.3'9''" + "\nB.8'2''" + "\nC.8'3''" + "\nD.Big Bird is not a character");
+			answer = scan.nextLine();
+			if (answer.equalsIgnoreCase("A")) {
+				correct();
+			}
+			else {
+				wrong();
+				break;
+			}
+			System.out.println("Congratulations, you have won Sesame Adventure!");
+			b = false;
+		}
+	}
+
+	public static void correct() {
+		System.out.println("Correct");
+	}
+	public static void wrong() {
+		System.out.println("Wrong! Elmo wins :(");
+	}
 }
