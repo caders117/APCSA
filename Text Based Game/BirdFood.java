ee6b4e9ad46f4ae303fc170e41b9719b24a2ca98
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-06 00:05:55 -0800

    Update BirdFood.java

--- Text Based Game/BirdFood.java
+++ Text Based Game/BirdFood.java
@@ -4,10 +4,34 @@
  * @author Jonathon Webster and Cade Richard
  * @version 2/23/18
  */
+
+/**
+ * This class represent the bird food object and extends Item
+ * @author Jonathon Webster and Cade Richard
+ * @version 2/23/18
+ */
 public class BirdFood extends Item {
-		
+	
+	private int amt;
+	
 	public BirdFood(int xPos, int yPos, int amount) {
 		super("Food", "Use to scare away Big Birds", xPos, yPos);
 		amt = amount;
+	}
+	
+	public void addFood(int amtNew) {
+		amt += amtNew;
+	}
+	
+	public void removeFood(int amtRem) {
+		amt -= amtRem;
+	}
+	
+	public int getAmount() {
+		return amt;
+	}
+	
+	public String toString() {
+		return super.toString() + "  Amount: " + amt;
 	}
 }
