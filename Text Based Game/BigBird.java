5b622bb44308a37ccae6de018d6621f34affe87e
jonathonwww <30909315+jonathonwww@users.noreply.github.com>
2018-03-05 23:17:45 -0800

    Update BigBird.java

--- Text Based Game/BigBird.java
+++ Text Based Game/BigBird.java
@@ -1,3 +1,8 @@
+/**
+ * This class has constructors to create a Big Bird, which usually holds goldfish.
+ * @author Jonathon Webster and Cade Richard
+ * @version 2/23/18
+ */
 public class BigBird extends Entity {
 
 	public BigBird(int xPos, int yPos) {
