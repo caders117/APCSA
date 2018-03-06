dbb55ea74532331bcdd10422b14e550542b3ab16
jonathonwww <30909315+jonathonwww@users.noreply.github.com>
2018-03-05 23:28:35 -0800

    Update Item.java

--- Text Based Game/Item.java
+++ Text Based Game/Item.java
@@ -1,4 +1,8 @@
-
+/**
+ * This class is the parent class of BirdFood and GoldFish
+ * @author Jonathon Webster and Cade Richard
+ * @version 2/23/18
+ */
 public abstract class Item extends NamedThing {
 	
 	public Item(String name, String desc, int xPos, int yPos) {
