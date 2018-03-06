49af0e81750629202c449ff5048a1e247bd67db9
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-06 00:12:14 -0800

    Update GoldFish.java

--- Text Based Game/GoldFish.java
+++ Text Based Game/GoldFish.java
@@ -4,26 +4,11 @@
  * @version 2/23/18
  */
 public class GoldFish extends Item {
-	private int amt;
 	
 	public GoldFish (int xPos, int yPos, int amount) {
 		super("Goldfish", "Food to access Elmo", xPos, yPos);
-		amt = amount;
+		setAmount(amount);
 	}
 	
-	public void setAmount(int amt) {
-		this.amt = amt;
-	}
 	
-	public int getAmount() {
-		return amt;
-	}
-	
-	public void addFood(int amt) {
-		this.amt += amt;
-	}
-	
-	public String toString() {
-		return super.toString() + "  Amount: " + amt;
-	}
 }
