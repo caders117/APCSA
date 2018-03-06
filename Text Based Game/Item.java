a566c73e142dd7b44730b6102768d46acd2aca78
caders117 <31219010+caders117@users.noreply.github.com>
2018-03-06 00:12:35 -0800

    Update Item.java

--- Text Based Game/Item.java
+++ Text Based Game/Item.java
@@ -5,8 +5,26 @@
  */
 public abstract class Item extends NamedThing {
 	
+	private int amt;
+	
 	public Item(String name, String desc, int xPos, int yPos) {
 		super(name, desc, xPos, yPos);
+	}
+	
+	public void setAmount(int amt) {
+		this.amt = amt;
+	}
+	
+	public int getAmount() {
+		return amt;
+	}
+	
+	public void addFood(int amt) {
+		this.amt += amt;
+	}
+	
+	public String toString() {
+		return super.toString() + "  Amount: " + amt;
 	}
 
 }
